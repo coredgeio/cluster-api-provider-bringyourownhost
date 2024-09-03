55c803f35cf06ff53bbca2d87f0027dfc55a9457
Parth Yadav <parthyadav3105@gmail.com>
2024-09-03 16:04:56 +0530

    Update to use Kubernetes version 1.31.0
        Fixes outdated images and apt repository to add support for k8s v1.31
        installation using bundles.
    
    Signed-off-by: Parth Yadav <parth@coredge.io>

--- installer/bundle_builder/ingredients/deb/download.sh
+++ installer/bundle_builder/ingredients/deb/download.sh
@@ -7,19 +7,20 @@ set -e
 
 echo  Update the apt package index and install packages needed to use the Kubernetes apt repository
 sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
 
 echo Download containerd
 curl -LOJR https://github.com/containerd/containerd/releases/download/v${CONTAINERD_VERSION}/cri-containerd-cni-${CONTAINERD_VERSION}-linux-amd64.tar.gz
 
 echo Download the Google Cloud public signing key
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://dl.k8s.io/apt/doc/apt-key.gpg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 echo Add the Kubernetes apt repository
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 echo Update apt package index, install kubelet, kubeadm and kubectl
 sudo apt-get update
 sudo apt-get download {kubelet,kubeadm,kubectl}:$ARCH=$KUBERNETES_VERSION
-sudo apt-get download kubernetes-cni:$ARCH=1.1.1-00
-sudo apt-get download cri-tools:$ARCH=1.25.0-00
+sudo apt-get download kubernetes-cni:$ARCH=1.5.0-2.1
+sudo apt-get download cri-tools:$ARCH=1.31.1-1.1
